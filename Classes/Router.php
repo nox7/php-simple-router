2525752bbab4be10ebafe8f0807f355621b432c2
nox7 <garetgreen1996@gmail.com>
2018-12-30 07:21:59 -0600

    Catch mysqli_sql_exception when uri_routes doesn't exist
    Show the necessary table to create

--- Classes/Router.php
+++ Classes/Router.php
@@ -12,9 +12,22 @@
 		public function getAllRoutes(){
 			// This became necessary to handle regularly-expressed uris in PHP
 			// Get all of the uri routes
-			$statement = $this->dbConnection->prepare("
-				SELECT * FROM uri_routes
-			");
+			try{
+				$statement = $this->dbConnection->prepare("
+					SELECT * FROM uri_routes
+				");
+			}catch(mysqli_sql_exception $e){
+				print("<h1>Missing URI Routes Database Table</h1>");
+				print("<p>Your selected database does not have a table name `uri_routes`. Please create one that follows this table:</p>");
+				print("<table><thead><tr><th>column name</th><th>type</th><th>coalition</th><th>index</th></tr></thead>");
+				print("<tbody><tr><td>uri</td><td>varchar 255</td><td>ascii_general</td><td>PRIMARY</td></tr>");
+				print("<tr><td>view</td><td>varchar 255</td><td>ascii_general</td><td></td></tr>");
+				print("<tr><td>layout</td><td>varchar 255</td><td>ascii_general</td><td></td></tr>");
+				print("<tr><td>isRegularExpression</td><td>int</td><td></td><td></td></tr>");
+				print("<tr><td>customData</td><td>text</td><td></td><td></td></tr>");
+				print("</tbody></table>");
+				exit();
+			}
 			$statement->execute();
 			$result = $statement->get_result();
 			return $result->fetch_all(MYSQLI_ASSOC);
